0bd1081bbfc7f13f23127ea2ee3fc1243308e47b
lordolli13-pixel <lord.olli13@gmail.com>
2026-07-01 13:11:48 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,14 @@
-const CACHE_NAME = 'planovac-v1';
+const cacheName = 'planovac-v1';
+const filesToCache = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png'
+];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['./', './index.html'])));
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)));
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
 });

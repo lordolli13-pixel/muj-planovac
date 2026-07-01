a147416eee8d2e16dd6ef2c14fa9e95448f93e63
lordolli13-pixel <lord.olli13@gmail.com>
2026-07-01 23:54:57 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
-<link rel="manifest" href="manifest.json">
+const CACHE_NAME = 'planner-2026-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './supabase.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).

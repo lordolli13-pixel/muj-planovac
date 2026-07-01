5ebd9dc4f9e469dcd89f8f40151654630b4dd300
lordolli13-pixel <lord.olli13@gmail.com>
2026-07-01 16:09:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,1 @@
-const CACHE_NAME = 'planner-v1';
-const ASSETS = [
-  './index.html'
-];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
-});
+self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));

20e8dab10b510fb473348e305a59a1fe3bf1dc38
lordolli13-pixel <lord.olli13@gmail.com>
2026-07-02 00:15:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,4 +17,13 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
